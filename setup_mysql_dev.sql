3a0937eec16b0c3286f0f1b50e69126b8aa81179
Nitsu47 <agustraversa2004@gmail.com>
2024-03-18 14:55:05 +0000

    Task 3 - MySQL setup development(Creation of database, user and privileges)

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- Creates a basic setup(database, user, psswd and privileges)
+-- Database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- User
+CREATE USER IF NOT EXISTS
+'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+-- Privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost';
+GRANT SELECT PRIVILEGE ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
