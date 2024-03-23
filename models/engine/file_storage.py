80b8057969ecbac389edee7a38bb483056d4a4ce
Nitsu47 <agustraversa2004@gmail.com>
2024-03-23 20:46:08 +0000

    correction of overidentation and correction in delete/file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -52,13 +52,17 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        if key in self.__objects:
-            del self.__objects[key]
+        """deletes a object from __objects"""
+        if obj is None:
+            pass
         else:
-            return
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
+            else:
+                return
