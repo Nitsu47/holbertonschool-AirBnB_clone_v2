8dc1f86c8319c4bd15524e857f91f03ea494ee69
Nitsu47 <agustraversa2004gmail.com>
2024-03-25 01:07:02 +0000

    update in City and State models(use of columns and relationship

--- models/city.py
+++ models/city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+
+    name = Column(str(128), nullable=False)
+
+    state_id = Column(str(60), nullable=False, foreignkey='states.id')
--- models/state.py
+++ models/state.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(str(128), nullable=False)
+
+    cities = relationship("City", backref="state",
+                          cascade='all, delete, delete-orphan')
