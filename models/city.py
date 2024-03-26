d62a3ab0385298d3ac26162b8bbe4ed2aefe1b72
AnitRodriguez <anitruthrodriguez@gmail.com>
2024-03-26 10:35:24 -0700

    task 2 console improvements

--- models/city.py
+++ models/city.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
 
-    name = Column(str(128), nullable=False)
+    name = Column(String(128), nullable=False)
 
-    state_id = Column(str(60), nullable=False, foreignkey='states.id')
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
