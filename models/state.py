51b8435e256edfb31724387a407c5e136b8d1530
AnitRodriguez <anitruthrodriguez@gmail.com>
2024-03-26 10:34:51 -0700

    task 2 console improvements

--- models/state.py
+++ models/state.py
@@ -2,13 +2,14 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
     __tablename__ = 'states'
 
-    name = Column(str(128), nullable=False)
+    name = Column(String(128), nullable=False)
 
     cities = relationship("City", backref="state",
                           cascade='all, delete, delete-orphan')
