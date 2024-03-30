e06dcce804aba269753f18f16d5c0ac2afa75096
AnitRodriguez <anitruthrodriguez@gmail.com>
2024-03-30 12:54:39 -0700

    fix cut error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,18 +2,22 @@
 
 """DBStorage Class """
 
-import os
 from models.base_model import BaseModel, Base
+import os
+from sqlalchemy import create_engine
+from sqlalchemy import orm
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.amenity import Amenity
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.review import Review
 from models.user import User
-from sqlalchemy import create_engine
-from sqlalchemy import orm
-from sqlalchemy.orm import sessionmaker, scoped_session
 
+HBNB_MYSQL_USER = "hbnb_dev"
+HBNB_MYSQL_PWD = "hbnb_dev_pwd"
+HBNB_MYSQL_HOST = "localhost"
+HBNB_MYSQL_DB = "hbnb_dev_db"
 
 
 class DBStorage():
@@ -24,47 +28,29 @@ class DBStorage():
 
     def __init__(self):
         """inits the attr for dbstorage"""
-
-        try:
-            user = os.getenv('HBNB_MYSQL_USER')
-            pwd = os.getenv('HBNB_MYSQL_PWD')
-            host = os.getenv('HBNB_MYSQL_HOST')
-            db = os.getenv('HBNB_MYSQL_DB')
-            self.__engine = create_engine(
-                f'mysql+mysqldb://{user}:{pwd}@{host}/{db}', pool_pre_ping=True)
-        except Exception as i:
-            print(f"Error during database engine initialization: {i}")
+        user = os.getenv('HBNB_MYSQL_USER')
+        pwd = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        db = os.getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine(
+            f'mysql+mysqldb://{user}:{pwd}@{host}/{db}', pool_pre_ping=True)
 
     def all(self, cls=None):
         """Returns a dictionary with all objects in the database."""
-    
+        if cls is None:
+            objects = self.__session.query().all()
+        else:
+            objects = self.__session.query(cls).all()
 
-        try:
-            all_classes = [User, State, City, Amenity, Place, Review]
-        
-            if cls is None:
-                objects = []
-                for cls in all_classes:
-                    objects += self.__session.query(cls).all()
-            else:
-                objects = self.__session.query(cls).all()
+        obj_dict = {}
+        for obj in objects:
+            obj_dict[f'{obj.__class__.__name__}.{obj.id}'] = obj
 
-            obj_dict = {}
-            for obj in objects:
-                obj_dict[f'{obj.__class__.__name__}.{obj.id}'] = obj
-
-            return obj_dict
-        except Exception as a:
-            print(f"Error during database query: {a}")
-            return {}
+        return obj_dict
 
     def new(self, obj):
         """adds objects to the current database session"""
-        if self.__session is not None:
-            self.__session.add(obj)
-        else:
-            print("Error, no data session.")
-            return
+        self.__session.add(obj)
 
     def save(self):
         """commit all changes of the current database session"""
@@ -77,14 +63,9 @@ class DBStorage():
 
     def reload(self):
         """Reloads database and create new session"""
+        Base.metadata.create_all(self.__engine)
 
-
-        try:        
-            Base.metadata.create_all(self.__engine)
-
-            factory = sessionmaker(bind=self.__engine,
+        factory = sessionmaker(bind=self.__engine,
                                expire_on_commit=False)
-            Session = scoped_session(factory)
-            self.__session = Session()
-        except Exception as r:
-            print(f"Error during database reload: {r}")
+        Session = scoped_session(factory)
+        self.__session = Session()
