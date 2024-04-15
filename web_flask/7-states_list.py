5c05581ce451b6f244b8e7bad5d7bdd1f0c08d57
Nitsu47 <agustraversa2004@gmail.com>
2024-04-15 01:46:33 +0000

    states are now ordered

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,15 +8,17 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_session():
-    """"""
+    """Teardown method to manage app context"""
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def state_html():
+    """Displat a html page with the states list"""
     from models.state import State
     states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+    ordered_states = sorted(states, key=lambda x: x.name)
+    return render_template("7-states_list.html", ordered_states=ordered_states)
 
 
 if __name__ == "__main__":
